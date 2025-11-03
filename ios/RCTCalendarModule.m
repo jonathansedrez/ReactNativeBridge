e6657e82c6feb678fada11bb7bed215f46f0297a
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-11-03 15:03:58 -0300

    feat: add cocoapod class

---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,15 @@
+//
+//  RCTCalendarModule.m
+//  
+//
+//  Created by Jonathan de Britto Sedrez on 03/11/25.
+//
+
+#import "RCTCalendarModule.h"
+
+@implementation RCTCalendarModule
+
+// To export a module named RCTCalendarModule
+RCT_EXPORT_MODULE();
+
+@end
